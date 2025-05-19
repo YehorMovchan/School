7a47fc57c0f98b7eefac51ce5de932a744f789d8
Yehor Movchan <egormch21@gmail.com>
2025-05-19 16:54:45 +0300

    fix history schedule endpoints

--- src/main/java/ua/dnu/myv/repository/view/VScheduleRepository.java
+++ src/main/java/ua/dnu/myv/repository/view/VScheduleRepository.java
@@ -14,13 +14,13 @@ public interface VScheduleRepository extends JpaRepository<ViewSchedule, Long> {
     @Query("select sch from ViewSchedule sch where sch.to is null and sch.dayOfWeek=?1")
     List<ViewSchedule> findActual(int day);
 
-    @Query("select s from ViewSchedule s where s.dayOfWeek=?2 and ?1 between s.from and s.to ")
+    @Query("select s from ViewSchedule s where s.dayOfWeek=?2 and ?1 between s.from and s.to or s.from<?1 and s.to is null")
     List<ViewSchedule> findHistoryByDayOfWeek(LocalDate day, Integer dayOfWeek);
 
-    @Query("select s from ViewSchedule s where s.teacherId=?2 and ?1 between s.from and s.to")
+    @Query("select s from ViewSchedule s where s.teacherId=?2 and ?1 between s.from and s.to or s.from<?1 and s.to is null")
     List<ViewSchedule> findHistoryByTeacherId(LocalDate day, Integer teacherId);
 
-    @Query("select s from ViewSchedule s where s.classField=?2 and ?1 between s.from and s.to")
+    @Query("select s from ViewSchedule s where s.classField=?2 and ?1 between s.from and s.to or s.from<?1 and s.to is null")
     List<ViewSchedule> findHistoryByClassField(LocalDate day, Integer classField);
 
     @Query("select vs from Kid k left join fetch ViewSchedule vs on k.grade=vs.classField where k.id=?1 and vs.to is null order by vs.dayOfWeek, vs.lessonNumber")
